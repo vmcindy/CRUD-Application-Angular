89998ff2e0722c64fce27ffc85c8ac4e0333d820
apiedy <abhishek.piedy@gmail.com>
2017-05-26 14:12:38 -0400

    Bug fixes

--- js/app.js
+++ js/app.js
@@ -1,10 +1,6 @@
 
 	var app = angular.module('myApp', ['ngRoute']);
 
-	/*app.controller('FormController',  function() {
-	this.a=10;
-});*/
-
 	app.config(function($routeProvider) {
 		$routeProvider
 			.when('/', {
